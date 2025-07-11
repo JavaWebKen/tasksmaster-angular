6c600d29a41e7550f2bd373ea34b5696a7695874
Ken Christie <Kenneth.christie@allpaid.com>
2025-07-11 10:38:53 -0600

    Cleaning up unused component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component, inject } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
-import { ProjectComponent } from './project/project.component';
 import { AuthService } from './services/auth.service';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ProjectComponent, RouterLink],
+  imports: [RouterOutlet,  RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
